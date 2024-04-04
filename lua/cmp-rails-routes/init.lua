20c63f829dbfbf04102330f0a977e434e4301b8c
Eike <eike.rackwitz@mail.de>
2024-04-04 22:34:05 +0200

    added .stylua.toml, and formatted accordingly

--- lua/cmp-rails-routes/init.lua
+++ lua/cmp-rails-routes/init.lua
@@ -1,8 +1,8 @@
-local debounce = require("telescope.debounce").debounce_leading
-local Job = require("plenary.job")
-local current_script_path = debug.getinfo(1, "S").source:sub(2)
-local root_path = current_script_path:gsub("/[^/]+/[^/]+/[^/]+$", "")
-local script_path = root_path .. "/scripts/rails_routes_to_json.rb"
+local debounce = require('telescope.debounce').debounce_leading
+local Job = require 'plenary.job'
+local current_script_path = debug.getinfo(1, 'S').source:sub(2)
+local root_path = current_script_path:gsub('/[^/]+/[^/]+/[^/]+$', '')
+local script_path = root_path .. '/scripts/rails_routes_to_json.rb'
 
 local items = {}
 local w = vim.loop.new_fs_event()
@@ -12,16 +12,16 @@ local function create_text_label_with_params(base_label, params)
     return base_label
   end
 
-  local label = base_label .. "("
+  local label = base_label .. '('
 
   for i, param in ipairs(params) do
-    label = label .. ":" .. param
+    label = label .. ':' .. param
     if i < #params then
-      label = label .. ", "
+      label = label .. ', '
     end
   end
 
-  return label .. ")"
+  return label .. ')'
 end
 
 local function create_snippet_label_with_params(base_label, params)
@@ -29,12 +29,12 @@ local function create_snippet_label_with_params(base_label, params)
     return base_label
   end
 
-  local label = base_label .. "(${1:" .. params[1] .. "}"
+  local label = base_label .. '(${1:' .. params[1] .. '}'
 
   for i = 2, #params do
-    label = label .. ", ${" .. i .. ":" .. params[i] .. "}"
+    label = label .. ', ${' .. i .. ':' .. params[i] .. '}'
   end
-  return label .. ")"
+  return label .. ')'
 end
 
 local function start_watching_routes()
@@ -52,62 +52,48 @@ local function start_watching_routes()
     end
 
     Job:new({
-      command = "bundle",
-      args = { "exec", "ruby", script_path },
+      command = 'bundle',
+      args = { 'exec', 'ruby', script_path },
       on_exit = function(j, return_val)
         if return_val == 0 then
-          local ok, routes = pcall(vim.json.decode, table.concat(j:result(), "\n"))
+          local ok, routes = pcall(vim.json.decode, table.concat(j:result(), '\n'))
           if ok then
             items = {}
             for _, route in ipairs(routes) do
-              local url_menu_label = create_text_label_with_params(
-                string.format("%s_url", route.route),
-                route.required_parts
-              )
-              local url_insert_text = create_snippet_label_with_params(
-                string.format("%s_url", route.route),
-                route.required_parts
-              )
+              local url_menu_label = create_text_label_with_params(string.format('%s_url', route.route), route.required_parts)
+              local url_insert_text = create_snippet_label_with_params(string.format('%s_url', route.route), route.required_parts)
               table.insert(items, {
                 label = url_menu_label,
                 kind = vim.lsp.protocol.CompletionItemKind.Method,
                 insertText = url_insert_text,
-                insertTextFormat = vim.lsp.protocol.InsertTextFormat
-                    .Snippet,
+                insertTextFormat = vim.lsp.protocol.InsertTextFormat.Snippet,
               })
-              local path_menu_label = create_text_label_with_params(
-                string.format("%s_path", route.route),
-                route.required_parts
-              )
-              local path_insert_text = create_snippet_label_with_params(
-                string.format("%s_path", route.route),
-                route.required_parts
-              )
+              local path_menu_label = create_text_label_with_params(string.format('%s_path', route.route), route.required_parts)
+              local path_insert_text = create_snippet_label_with_params(string.format('%s_path', route.route), route.required_parts)
               table.insert(items, {
                 label = path_menu_label,
                 kind = vim.lsp.protocol.CompletionItemKind.Method,
                 insertText = path_insert_text,
-                insertTextFormat = vim.lsp.protocol.InsertTextFormat
-                    .Snippet,
+                insertTextFormat = vim.lsp.protocol.InsertTextFormat.Snippet,
               })
             end
           else
-            print("error parsing JSON file")
+            print 'error parsing JSON file'
           end
         else
-          print("ruby-script failed with return_val:", return_val)
+          print('ruby-script failed with return_val:', return_val)
         end
       end,
     }):start()
   end, 1000)
 
   watch_file = function(fname)
-    local fullpath = vim.api.nvim_call_function("fnamemodify", { fname, ":p" })
+    local fullpath = vim.api.nvim_call_function('fnamemodify', { fname, ':p' })
     w:start(fullpath, { recursive = true }, on_change)
   end
 
   on_change(nil)
-  watch_file("config/")
+  watch_file 'config/'
 end
 
 local function stop_watching_routes()
@@ -122,17 +108,14 @@ source.complete = function(self, _, callback)
 end
 
 source.is_available = function()
-  return vim.bo.filetype == "eruby"
-      or vim.bo.filetype == "slim"
-      or vim.bo.filetype == "haml"
-      or vim.bo.filetype == "ruby"
+  return vim.bo.filetype == 'eruby' or vim.bo.filetype == 'slim' or vim.bo.filetype == 'haml' or vim.bo.filetype == 'ruby'
 end
 
-vim.api.nvim_create_autocmd("BufEnter", {
-  desc = "Searching for rails routes to provide helper names",
-  group = vim.api.nvim_create_augroup("rails-route-helpers", { clear = true }),
+vim.api.nvim_create_autocmd('BufEnter', {
+  desc = 'Searching for rails routes to provide helper names',
+  group = vim.api.nvim_create_augroup('rails-route-helpers', { clear = true }),
   callback = function()
-    if vim.fn.filereadable("Gemfile") > 0 then
+    if vim.fn.filereadable 'Gemfile' > 0 then
       start_watching_routes()
     end
   end,
